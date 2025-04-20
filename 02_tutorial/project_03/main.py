78ef2e0d8b7e9b09da43acdfe6dbcc0a5602733f
NahumFGz <ngutierrez2507@gmail.com>
2025-04-20 02:18:46 -0500

    feat: :sparkles: Se agrega el validador de pydantic y el post

--- 02_tutorial/project_03/main.py
+++ 02_tutorial/project_03/main.py
@@ -3,6 +3,7 @@ from typing import Annotated
 import models
 from database import SessionLocal, engine
 from fastapi import Depends, FastAPI, HTTPException, Path, status
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 
 app = FastAPI()
@@ -21,6 +22,13 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+
 @app.get("/")
 async def read_all(db: Annotated[Session, Depends(get_db)]):
     return db.query(models.Todos).all()
@@ -34,3 +42,13 @@ async def read_todo(db: db_dependency, todo_id: Annotated[int, Path(gt=0)]):
         return todo_model
 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No se encontro la tarea")
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = models.Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
+
+    return {"message": "Tarea creada correctamente"}
