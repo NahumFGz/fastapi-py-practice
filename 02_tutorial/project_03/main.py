bea375f07578eab54fde33499d07f3007bfc40c3
NahumFGz <ngutierrez2507@gmail.com>
2025-04-20 02:39:59 -0500

    feat: :sparkles: Agregando update and delete

--- 02_tutorial/project_03/main.py
+++ 02_tutorial/project_03/main.py
@@ -52,3 +52,35 @@ async def create_todo(db: db_dependency, todo_request: TodoRequest):
     db.commit()
 
     return {"message": "Tarea creada correctamente"}
+
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def update_todo(
+    db: db_dependency, todo_id: Annotated[int, Path(gt=0)], todo_request: TodoRequest
+):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo no encontrado")
+
+    todo_model.title = todo_request.title
+    todo_model.description = todo_model.description
+    todo_model.priority = todo_request.priority
+    todo_model.complete = todo_request.complete
+
+    # db.add(todo_model)  # SQLalchemy detecta que es un update o create, no es necesario ponerlo
+    db.commit()
+
+    return {"message": "Actualización correcta"}
+
+
+@app.delete("/todo/{todo_id}")
+async def delete_todo(db: db_dependency, todo_id: Annotated[int, Path(gt=0)]):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    if todo_model is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo no encontrado")
+
+    db.query(models.Todos).filter(models.Todos.id == todo_id).delete()
+    db.commit()
+
+    return {"message": "Eliminación correcta"}
