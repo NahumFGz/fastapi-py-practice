748e4eb8530a14a4eb996211205f4a5545fd33e7
NahumFGz <ngutierrez2507@gmail.com>
2025-04-20 02:06:53 -0500

    feat: :sparkles: Add get by id

--- 02_tutorial/project_03/main.py
+++ 02_tutorial/project_03/main.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 import models
 from database import SessionLocal, engine
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException, Path, status
 from sqlalchemy.orm import Session
 
 app = FastAPI()
@@ -18,9 +18,19 @@ def get_db():
         db.close()
 
 
-#! db_dependency = Annotated[Session, Depends(get_db)]
+db_dependency = Annotated[Session, Depends(get_db)]
 
 
 @app.get("/")
 async def read_all(db: Annotated[Session, Depends(get_db)]):
     return db.query(models.Todos).all()
+
+
+@app.get("/todo/{todo_id}")
+async def read_todo(db: db_dependency, todo_id: Annotated[int, Path(gt=0)]):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    if todo_model is not None:
+        return todo_model
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No se encontro la tarea")
