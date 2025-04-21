a8a74f1fdd54db4659e0892c0de4fbb5bfe29f54
NahumFGz <ngutierrez2507@gmail.com>
2025-04-21 01:19:00 -0500

    feat: :sparkles: Agreagndo user routers

--- 02_tutorial/project_03/main.py
+++ 02_tutorial/project_03/main.py
@@ -3,7 +3,7 @@ from typing import Annotated
 import models
 from database import engine
 from fastapi import FastAPI
-from routers import admin, auth, todos
+from routers import admin, auth, todos, users
 
 app = FastAPI()
 
@@ -13,3 +13,4 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router, prefix="/todo")
 app.include_router(admin.router)
+app.include_router(users.router)
